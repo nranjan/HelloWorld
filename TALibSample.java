0517d747da24bc43a851807374abe6d07b6d2b9f
nranjan <nishant.ranjan@effte.co>
2014-05-01 07:24:07 +0530

    Update and rename Max.java to TALibSample.java

--- TALibSample.java
+++ TALibSample.java
@@ -10,27 +10,25 @@ import com.breezetrader.Tick;
 *  http://docs.breezetrader.com
 *
 */
-public class Max extends Strategy {
+public class TALibSample extends Strategy {
 	
-	boolean flag = false;
-	String symbol = "EUR.USD";
+	String symbol = "RELIANCE";
 	/*
 	*  initialize your context, 
 	*  technical indicators other variables
 	*/
 	public void initialize(Context context)
 	{	
-			initTALib("macd","macd1", "12", "26","9", "EU0009652759", "currentValueDouble" );
+			initTALib("macd","macd1", "12", "26","9", symbol, "currentValueDouble" );
 			initTALib("rsi","rsi1", "12", "EU0009652759", "currentValueDouble" );
+			
+			
 			context.setDataFrequency(2, Context.Frequency.WEEK);
-			context.setSymbols("EU0009652759");
-			context.setDataURL("https://s3-ap-southeast-1.amazonaws.com/breezetrader-test/EU0009652759.csv");
+			context.setSymbols(symbol);
 			context.setPortfolioValue(BigDecimal.valueOf(100000));
 			context.setDataType(Event.Type.BAR);
 			context.setStartDate("03-9-2012");
 			context.setEndDate("31-10-2012");
-			log("Initialized");
-			
 	}
 	
 	/*
@@ -43,29 +41,14 @@ public class Max extends Strategy {
 	{
 		double macd1 = getData("macd1" , "macd");
 		double macd1Hist = getData("macd1" , "macdhist");
+		double macd1Sig = getData("maccd1","macdsig")
 		double rsi1 = getData("rsi1");
-//		log("macd1: "+macd1);
-//		log("macd1hist: "+macd1Hist);
-//		log("macd1sig: "+macd1Sig);
-		log("current Time: "+getTimeStamp());
-		if(rsi1>70)
-			log("rsi > 70");
-		else if(rsi1<30)
-			log("rsi < 30");
-		else
-			log("rsi = "+rsi1);
-		if(macd1 > macd1Hist)
-		{
-			log("ID: "+order(OrderType.Market,symbol, 10));
-			
-			log("openpositions: "+getPosition(symbol));
-		}
-		else
-		{
-			log("openpositions: "+getPosition(symbol));
-			closeAllPositions(symbol);
-			
-		}
+		
+		
+		log("macd1: "+macd1);
+		log("macd1hist: "+macd1Hist);
+		log("macd1sig: "+macd1Sig);
+		
 		
 	}
 	
